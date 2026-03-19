9bf6ecfe52c58a57ab8f835262c88b865317e3c4
Jaaron Cabsaban <jaaron4401@gmail.com>
2026-03-18 20:38:21 -1000

    Player State Script

--- RealGame/Assets/Scripts/PlayerLetterCollector.cs
+++ RealGame/Assets/Scripts/PlayerLetterCollector.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+
+
 public class PlayerLetterCollector : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
---
+++ RealGame/Assets/Scripts/PlayerState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public int maxHP;
+    public int CurrentHP;
+
+    public char[] usableLetters;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
